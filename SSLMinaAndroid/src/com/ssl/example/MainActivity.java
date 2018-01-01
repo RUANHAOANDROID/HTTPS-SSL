1552db10eb922322bfc63c5728e883528019d944
RH <52927295@qq.com>
2018-01-01 12:29:25 +0800

    Certificate and code
    Certificate and code

---
+++ SSLMinaAndroid/src/com/ssl/example/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ssl.example;
+
+import com.example.sslminaandroid.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+/**
+ * 
+ * @author rh
+ *
+ */
+public class MainActivity extends Activity {
+
+	private TextView mTextMessage;
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Intent intent = new Intent(MainActivity.this, MyService.class);
+		stopService(intent);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		mTextMessage = (TextView) findViewById(R.id.message);
+		Intent intent = new Intent(MainActivity.this, MyService.class);
+		startService(intent);
+	}
+
+}
---
+++ SSLMinaAndroid/src/com/ssl/example/MyService.java
@@ -0,0 +1,32 @@
+package com.ssl.example;
+
+import com.ssl.example.mina.MinaClient;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by hao.ruan on 2017/12/21.
+ */
+
+public class MyService extends Service {
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return null;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				new MinaClient();
+			}
+		});
+		thread.start();
+	}
+}
